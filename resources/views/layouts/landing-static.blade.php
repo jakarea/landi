9720d809345a42261f2d5f22d1a737bd39e654b8
Jakarea <jakareaparvez@gmail.com>
2025-09-01 12:27:02 +0600

    landing page styles fixed

--- resources/views/layouts/landing-static.blade.php
+++ resources/views/layouts/landing-static.blade.php
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="bn">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Landing Page')</title>
-    <link rel="stylesheet" href="{{ asset('razu-landing/dist/assets/index-BdpMqcnK.css') }}">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&display=swap"
+    rel="stylesheet">
+
+
+   <title>@yield('title', 'Landing Page')</title>
+  <link rel="stylesheet" href="{{ asset('/assets/landing/index-BdpMqcnK.css') }}">
 </head>
-<body>
+
+<body class="bg-body">
+
     @yield('content')
 </body>
 </html>
